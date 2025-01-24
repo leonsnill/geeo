970cfa25a6975b5e2e1cb5c779e31579b04b38db
leonsnill <leon.nill@geo.hu-berlin.de>
2025-01-24 18:34:31 +0100

    Fix image dimension calculation to use rounding for improved accuracy

--- geeo/misc/spacetime.py
+++ geeo/misc/spacetime.py
@@ -323,7 +323,7 @@ def get_crs_transform_and_img_dimensions(input_roi, pix_res):
     crs_transform = [pix_res, 0, xmin, 0, pix_res, ymin]
 
     # calculate image dimensions
-    img_dimensions = str(int(math.ceil((xmax - xmin) // pix_res)))+"x"+str(int(math.ceil((ymax - ymin) // pix_res)))
+    img_dimensions = str(int(round((xmax - xmin) / pix_res)))+"x"+str(int(round((ymax - ymin) / pix_res)))
     
     return crs_transform, img_dimensions
 
